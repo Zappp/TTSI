530259e8e2c1c3698e19cfd669e614a9cf5524e5
Marcin <marcinkomar97@gmail.com>
2021-10-17 16:59:19 +0200

    minor changes

--- strona/delete_users.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('includes/config.php');
-$id=$_GET['del'];
-mysqli_query($con, "DELETE FROM users WHERE id = '$id'");
-header("Location:admin-panel.php");
-mysqli_close($con);
-
-?>
--- strona/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION['zalogowany']= false;
-session_destroy();
-
-header("location:login.php");
-setcookie('name','',time()-3000);
-?>
